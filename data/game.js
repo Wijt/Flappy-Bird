ac6ec4f8926a7e07fe2b3cfc676b485d51fdf26a
Furkan Kaya <8fkaya@gmail.com>
2020-06-04 17:18:44 +0300

    Sounds effects added.
    Point, fall, wing and hit effects added.

--- data/game.js
+++ data/game.js
@@ -3,6 +3,12 @@ let gravity = 1;
 
 let pipes = [];
 
+let soundEffects = [];
+
+function preload() {
+    LoadSounds();
+}
+
 function setup(){
     //createCanvas(288,512);
     createCanvas(windowWidth, windowHeight)
@@ -13,6 +19,20 @@ function setup(){
         new Pipe(width + i * PIPE_BETWEEN + PIPE_WIDTH, random(150, height-150));
     }
 }
+
+function LoadSounds(){
+    soundEffects.push(loadSound('data/sound/sfx_point.wav'));
+    soundEffects.push(loadSound('data/sound/sfx_die.wav'));
+    soundEffects.push(loadSound('data/sound/sfx_hit.wav'));
+    soundEffects.push(loadSound('data/sound/sfx_swooshing.wav'));
+    soundEffects.push(loadSound('data/sound/sfx_wing.wav'));
+}
+
+
+
+
+
+
 
 function draw(){
     pop();
@@ -48,10 +68,13 @@ function update() {
             element.update();
             if (element.isCollide(bird)){
                 bird.live = false;
+                try { soundEffects[2].play(); } catch(e) {}
                 console.warn("ÇARPTI");
+                try { soundEffects[1].play(); } catch(e) {}
             }
             if (element.pos.x <= bird.pos.x && element.hasPoint){
                 element.hasPoint = false;
+                try { soundEffects[0].play(); } catch(e) {}
                 bird.point++;
             }
         });
@@ -59,8 +82,10 @@ function update() {
 }
 
 function mouseReleased(){
-    if (bird.live)
+    if (bird.live){
         bird.jump();
+        try { soundEffects[4].play(); } catch(e) {}
+    }
 }
 
 
@@ -86,7 +111,6 @@ function circleRect(bird, rect) {
     let distX = bird.pos.x - testX;
     let distY = bird.pos.y - testY;
     let distance = sqrt((distX*distX) + (distY*distY));
-    console.log(distance, bird.radius);
     // if the distance is less than the bird.radius, collision!
     if (distance <= bird.radius-5) {
       return true;
